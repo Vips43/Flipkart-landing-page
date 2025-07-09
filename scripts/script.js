eba02bc68f7f63b78ce4ddaf7cfc539351d611fd
Vips43 <vipulgehlot43@gmail.com>
2025-07-09 19:50:21 +0530

    update

--- scripts/script.js
+++ scripts/script.js
@@ -5,7 +5,14 @@ let menuBar = document.querySelector('#menubar')
 
 function clickMenu(){
 
-  sideMenu.classList.toggle('display')
-
+  // sideMenu.classList.toggle('display')
+  if(sideMenu.classList.contains('display')){
+    sideMenu.classList.remove('display')
+    menuBar.classList.remove('fa-xmark')
+  }
+  else{
+    sideMenu.classList.add('display')
+    menuBar.classList.add('fa-xmark')
+  }
 }
 
